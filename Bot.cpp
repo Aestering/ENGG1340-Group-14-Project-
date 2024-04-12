ec74f1c8b8ee779ab1a6970f54ac5c9564deb039
Ismayil06 <u3620545@connect.hku.hk>
2024-04-12 13:40:55 +0800

    Difficulty correction

--- Bot.cpp
+++ Bot.cpp
@@ -21,7 +21,7 @@ void Bot::update_direction(int appleX, int appleY) {
     int botX = x[0];
     int botY = y[0];
     srand(time(NULL));
-    int flag = rand() % 4;
+    int flag = rand() % 5;
     //flag = 1;
     int distanceX = appleX - botX;
     int distanceY = appleY - botY;
